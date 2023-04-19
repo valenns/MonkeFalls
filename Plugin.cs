49b798fff9381d49e99fa40afc762e1c628be1a1
valenns <131170856+valenns@users.noreply.github.com>
2023-04-19 10:35:17 +0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+using GorillaLocomotion;
+
+namespace MonkeFalls
+{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		MeshCollider floor;
+
+		Rigidbody monke;
+
+		void Start()
+		{
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnEnable()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			floor = GameObject.Find("Level/forest/ForestObjects/pitgeo/pit ground").GetComponent<MeshCollider>();
+			monke = Player.Instance.bodyCollider.attachedRigidbody;
+		}
+
+		void Update()
+		{
+			if (monke != null && floor != null)
+			{
+				if (monke.velocity.y < -7f && floor.bounds.Contains(monke.position))
+				{
+					Application.Quit();
+				}
+			}
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeFalls
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.Valen.gorillatag.MonkeFalls";
+		public const string Name = "MonkeFalls";
+		public const string Version = "1.0.0";
+	}
+}
